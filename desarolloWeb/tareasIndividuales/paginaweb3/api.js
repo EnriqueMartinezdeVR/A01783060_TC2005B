886205006934860d056a88f1ca8704d5a06b5578
Enrique Martinez <a01783060@tec.mx>
2024-05-26 21:53:36 -0600

    tarea Actividad: Página web simple

---
+++ desarolloWeb/tareasIndividuales/paginaweb3/api.js
@@ -0,0 +1,23 @@
+//Api enrique martínez de velasco
+'use strict';
+
+import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'htmldepaginaweb.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}/`);
+});
+
